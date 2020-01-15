fdeea268cc67d970d8a34f12d880fdb0deaa8377
Bill Liu <billliusiyi@gmail.com>
2020-01-15 11:42:33 -0500

    React Game
    tic tac toe setup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,106 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//first class
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Square extends React.Component {
+    render() {
+      return (
+        <button 
+            className="square" 
+            onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
+        </button>
+      );
+    }
+  }
+
+
+
+
+
+//second class
+
+  class Board extends React.Component {
+      constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null)
+        };
+      }
+
+      handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
+
+      renderSquare(i) {
+          return (<Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
+          />
+        );
+      }
+
+      render() {
+          const status = 'Next Player: X';
+
+          return (
+              <div>
+                <div className="Status">{status}</div>
+                <div className="row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+              </div>
+          );
+      }
+  }
+  
+
+
+
+
+  //third class
+
+  class Game extends React.Component {
+      render() {
+          return(
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>
+                        {/* scripts here*/}
+                    </div>
+                    <ol>
+                        {/* list stuff*/}
+                    </ol>
+                </div>
+            </div>
+          );
+      }
+  }
+  
+  // ========================================
+  
+  ReactDOM.render(
+    <Game />,
+    document.getElementById('root')
+  );
+  
